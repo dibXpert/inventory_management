8c8eb5e05f57f614cba04728e60c80b9027a25c3
Dib <93922964+dibXpert@users.noreply.github.com>
2024-08-28 06:10:34 +0800

    bootstrap, index page nav bar

--- prodxtrack/inventory/urls.py
+++ prodxtrack/inventory/urls.py
@@ -2,8 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('', views.inventory_list, name='inventory_list'),
-   path('add/', views.add_inventory, name='add-inventory'),
+   path('', views.index, name='index'), 
+   path('inventory/', views.inventory_list, name='inventory_list'),
+   #crud inventory
+   path('add/', views.add_inventory, name='add_inventory'),
+   path('edit/<int:pk>/', views.edit_inventory, name='edit_inventory'),
+   path('delete/<int:pk>/', views.delete_inventory, name='delete_inventory'),
+   
+   
+   #inbound
    path('inbound/',views.record_inbound, name='record_inbound'),
+   #outbound
    path('outbound/', views.record_outbound, name='record_outbound'), 
 ]
--- prodxtrack/inventory/views.py
+++ prodxtrack/inventory/views.py
@@ -1,14 +1,22 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect, get_object_or_404
 from .models import Inventory,Inbound,Outbound
 from .forms import InventoryForm, InboundForm, OutboundForm
 # Create your views here.
 
+
+
+
+def index(request):
+    return render(request, 'inventory/index.html')
+
+# CRUD inventory
 def inventory_list(request):
     inventory_items = Inventory.objects.all()
     context = {
         'inventory_items':inventory_items,
     }
     return render(request, 'inventory/inv_list.html', context)
+
 
 def add_inventory(request):
     if request.method == 'POST':
@@ -23,6 +31,32 @@ def add_inventory(request):
         'form': form
     }
     return render(request, 'inventory/add_inv.html',context)
+
+def edit_inventory(request, pk):
+    item = get_object_or_404(Inventory, pk=pk)
+    if request.method == 'POST':
+        form = InventoryForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect('inventory_list')
+    else:
+        form = InventoryForm(instance=item)
+    
+    context = {
+        'item':item,
+    }
+    return render(request, 'inventory/edit_inv.html', context)
+        
+def delete_inventory(request,pk):
+    item = get_object_or_404(Inventory, pk=pk)
+    if request.method == 'POST':
+        item.delete()
+        return redirect('inventory_list')
+    
+    context = {
+        'item': item
+    }
+    return render(request, 'inventory/delete_inv.html', context)
 
 # INBOUND
 def record_inbound(request):
