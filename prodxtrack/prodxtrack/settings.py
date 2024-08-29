578586a81bbcfb3398ea71af06c18d7e7c7d93f7
Dib <93922964+dibXpert@users.noreply.github.com>
2024-08-30 07:02:16 +0800

    button styling

--- prodxtrack/prodxtrack/settings.py
+++ prodxtrack/prodxtrack/settings.py
@@ -123,6 +123,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
+LOGIN_URL = 'login' 
 LOGIN_REDIRECT_URL = 'index'  # Redirect to the home page after login
 LOGOUT_REDIRECT_URL = 'login'  # Redirect to the login page after logout
